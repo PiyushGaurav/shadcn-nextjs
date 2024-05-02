481692946231da3bf47aa52229de633b80097e6b
PiyushGaurav <piyushgaurav07@gmail.com>
2024-05-02 18:09:16 +0530

    crash fix

---
+++ app/task-board/new/error.tsx
@@ -0,0 +1,10 @@
+'use client';
+export default function Error() {
+	return (
+		<div className="flex flex-col m-4 max-w-screen-xl px-4 mx-auto">
+			<div className="flex flex-col w-full justify-between items-center">
+				<h1 className="text-3xl font-bold my-10">Invalid Input!</h1>
+			</div>
+		</div>
+	);
+}
