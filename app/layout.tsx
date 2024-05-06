2c087d219fcb0dd173b84349f92cf1debb461a5b
PiyushGaurav <piyushgaurav07@gmail.com>
2024-05-06 21:33:19 +0530

    removing unused import

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import { Toaster } from '@/components/ui/sonner';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-	title: 'Shadcn Tutorial',
+	title: 'Task Board',
 	description: 'Created by Piyush'
 };
 
--- app/task-board/new/page.tsx
+++ app/task-board/new/page.tsx
@@ -1,12 +1,4 @@
 import AddEditForm from '../../../components/custom/AddEditForm';
-import {
-	Breadcrumb,
-	BreadcrumbItem,
-	BreadcrumbLink,
-	BreadcrumbList,
-	BreadcrumbPage,
-	BreadcrumbSeparator
-} from '@/components/ui/breadcrumb';
 export default function New() {
 	return (
 		<div className="flex flex-col ">
